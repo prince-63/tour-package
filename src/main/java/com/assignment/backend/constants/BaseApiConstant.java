e1f38cad7834682ea75950009b7bc4d39787c6cc
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-10 21:57:11 +0530

    feat: add api constants

---
+++ src/main/java/com/assignment/backend/constants/BaseApiConstant.java
@@ -0,0 +1,8 @@
+package com.assignment.backend.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public final class BaseApiConstant {
+    public static final String BASE_URL = "/api/v1/";
+}
---
+++ src/main/java/com/assignment/backend/constants/TourPackageApiConstants.java
@@ -0,0 +1,8 @@
+package com.assignment.backend.constants;
+
+public final class TourPackageApiConstants {
+    private static final String BASE_TOUR_PACKAGE = BaseApiConstant.BASE_URL + "tour-package/";
+    public static final String ADD_TOUR_PACKAGE = BASE_TOUR_PACKAGE + "add";
+    public static final String GET_ALL_TOUR_PACKAGE = BASE_TOUR_PACKAGE + "all";
+    public static final String GET_TOUR_PACKAGE_BY_ID = BASE_TOUR_PACKAGE + "{id}";
+}
