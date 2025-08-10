903d9fa0adb34444ec5ce125da270a60b13f60ba
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-10 20:44:25 +0530

    refactor: rename file name for better flow

--- src/main/java/com/assignment/backend/mapper/TourPackageMapper.java
+++ src/main/java/com/assignment/backend/mapper/TourPackageMapper.java
@@ -1,32 +1,31 @@
 package com.assignment.backend.mapper;
 
-import com.assignment.backend.dto.TourRequestDTO;
-import com.assignment.backend.dto.TourResponseDTO;
+import com.assignment.backend.dto.TourPackageRequestDTO;
+import com.assignment.backend.dto.TourPackageResponseDTO;
 import com.assignment.backend.entities.TourPackage;
 
 public class TourPackageMapper {
 
-    public static TourPackage toEntity(TourRequestDTO tourRequestDTO) {
+    public static TourPackage toEntity(TourPackageRequestDTO tourPackageRequestDTO) {
         TourPackage tourPackage = new TourPackage();
-        tourPackage.setTitle(tourRequestDTO.getTitle());
-        tourPackage.setDescription(tourRequestDTO.getDescription());
-        tourPackage.setLocation(tourRequestDTO.getLocation());
-        tourPackage.setDuration(tourRequestDTO.getDuration());
-        tourPackage.setDiscountInPercentage(tourRequestDTO.getDiscountInPercentage());
-        tourPackage.setActualPrice(tourRequestDTO.getActualPrice());
+        tourPackage.setTitle(tourPackageRequestDTO.getTitle());
+        tourPackage.setDescription(tourPackageRequestDTO.getDescription());
+        tourPackage.setLocation(tourPackageRequestDTO.getLocation());
+        tourPackage.setDuration(tourPackageRequestDTO.getDuration());
+        tourPackage.setPercentageDiscount(tourPackageRequestDTO.getPercentageDiscount());
+        tourPackage.setActualPrice(tourPackageRequestDTO.getActualPrice());
         return tourPackage;
     }
 
-    public static TourResponseDTO toResponseDTO(TourPackage tourPackage) {
-        TourResponseDTO tourResponseDTO = new TourResponseDTO();
-        tourResponseDTO.setId(tourPackage.getId());
-        tourResponseDTO.setTitle(tourPackage.getTitle());
-        tourResponseDTO.setDescription(tourPackage.getDescription());
-        tourResponseDTO.setLocation(tourPackage.getLocation());
-        tourResponseDTO.setDuration(tourPackage.getDuration());
-        tourResponseDTO.setDiscountInPercentage(tourPackage.getDiscountInPercentage());
-        tourResponseDTO.setActualPrice(tourPackage.getActualPrice());
-        tourResponseDTO.setDiscountedPrice(tourPackage.getDiscountedPrice());
-        return tourResponseDTO;
+    public static TourPackageResponseDTO toResponseDTO(TourPackage tourPackage) {
+        TourPackageResponseDTO tourPackageResponseDTO = new TourPackageResponseDTO();
+        tourPackageResponseDTO.setId(tourPackage.getId());
+        tourPackageResponseDTO.setTitle(tourPackage.getTitle());
+        tourPackageResponseDTO.setDescription(tourPackage.getDescription());
+        tourPackageResponseDTO.setLocation(tourPackage.getLocation());
+        tourPackageResponseDTO.setDuration(tourPackage.getDuration());
+        tourPackageResponseDTO.setPercentageDiscount(tourPackage.getPercentageDiscount());
+        tourPackageResponseDTO.setActualPrice(tourPackage.getActualPrice());
+        return tourPackageResponseDTO;
     }
 }
