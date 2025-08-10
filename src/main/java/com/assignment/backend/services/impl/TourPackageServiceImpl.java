3a0cc3c77580b454c7977049e9c84cdcead6ce14
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-10 21:58:38 +0530

    feat: implement tour package service

---
+++ src/main/java/com/assignment/backend/services/impl/TourPackageServiceImpl.java
@@ -0,0 +1,58 @@
+package com.assignment.backend.services.impl;
+
+import com.assignment.backend.dto.TourPackageRequestDTO;
+import com.assignment.backend.dto.TourPackageResponseDTO;
+import com.assignment.backend.entities.TourPackage;
+import com.assignment.backend.exceptions.TourPackageNotFoundException;
+import com.assignment.backend.mapper.TourPackageMapper;
+import com.assignment.backend.repositories.TourPackageRepository;
+import com.assignment.backend.services.TourPackageService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TourPackageServiceImpl implements TourPackageService {
+
+    private final TourPackageRepository tourPackageRepository;
+
+    @Override
+    public TourPackageResponseDTO addTourPackage(TourPackageRequestDTO tourPackageRequestDTO) {
+        TourPackage tourPackage = TourPackageMapper.toEntity(tourPackageRequestDTO);
+        TourPackage saveTourPackage = tourPackageRepository.save(tourPackage);
+        TourPackageResponseDTO tourPackageResponseDTO = TourPackageMapper.toResponseDTO(saveTourPackage);
+        tourPackageResponseDTO.setDiscountedPrice(calculateDiscountedPrice(tourPackageResponseDTO.getActualPrice(), tourPackageResponseDTO.getPercentageDiscount()));
+        return tourPackageResponseDTO;
+    }
+
+    @Override
+    public List<TourPackageResponseDTO> getAllTourPackages() {
+        List<TourPackage> tourPackages = tourPackageRepository.findAll();
+        return tourPackages.stream().map(tourPackage -> {
+            BigDecimal discount = calculateDiscountedPrice(tourPackage.getActualPrice(), tourPackage.getPercentageDiscount());
+            TourPackageResponseDTO tourPackageResponseDTO = TourPackageMapper.toResponseDTO(tourPackage);
+            tourPackageResponseDTO.setDiscountedPrice(discount);
+            return tourPackageResponseDTO;
+        }).toList();
+    }
+
+    @Override
+    public TourPackageResponseDTO getTourPackageById(Long id) {
+        TourPackage tourPackage = tourPackageRepository.findById(id).orElseThrow(() -> new TourPackageNotFoundException(id));
+        TourPackageResponseDTO tourPackageResponseDTO = TourPackageMapper.toResponseDTO(tourPackage);
+        tourPackageResponseDTO.setDiscountedPrice(calculateDiscountedPrice(tourPackageResponseDTO.getActualPrice(), tourPackageResponseDTO.getPercentageDiscount()));
+        return tourPackageResponseDTO;
+    }
+
+    private BigDecimal calculateDiscountedPrice(BigDecimal actualPrice, Integer discountInPercentage) {
+        BigDecimal discount = actualPrice
+                .multiply(BigDecimal.valueOf(discountInPercentage))
+                .divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+
+        return actualPrice.subtract(discount);
+    }
+}
