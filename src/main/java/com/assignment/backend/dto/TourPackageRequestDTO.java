84f6fff1e0eeb68fb030038d4637c3d80a778074
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-10 22:21:46 +0530

    feat: add request validation

--- src/main/java/com/assignment/backend/dto/TourPackageRequestDTO.java
+++ src/main/java/com/assignment/backend/dto/TourPackageRequestDTO.java
@@ -1,5 +1,8 @@
 package com.assignment.backend.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,16 +10,23 @@ import java.math.BigDecimal;
 @Data
 public class TourPackageRequestDTO {
 
+    @NotEmpty(message = "Title should not be empty!")
     private String title;
 
+    @NotEmpty(message = "Description should not be empty!")
+    @Size(max = 1000, message = "The description length must be under 1000 words!")
     private String description;
 
+    @NotEmpty(message = "Location should not be empty!")
     private String location;
 
+    @NotEmpty(message = "Duration should not be empty!")
     private String duration;
 
+    @NotNull(message = "Discount percentage should not be null!")
     private Integer percentageDiscount;
 
+    @NotNull(message = "Price not be null!")
     private BigDecimal actualPrice;
 
 }
