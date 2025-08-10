b2fc50405dd742740db4d626ceb729c16fe89ea2
Prince Kumar Prasad <prince08833@gmail.com>
2025-08-10 21:57:50 +0530

    feat: add response message constants

---
+++ src/main/java/com/assignment/backend/constants/ErrorMessageConstants.java
@@ -0,0 +1,7 @@
+package com.assignment.backend.constants;
+
+public final class ErrorMessageConstants {
+    public static final String METHOD_ARGUMENT_FAILED = "Plz provide a valid input data.";
+    public static final String TOUR_PACKAGE_NOT_FOUND = "TOUR PACKAGE NOT FOUND";
+    public static final String INTERNAL_SERVER_ERROR = "Something went wrong during operation";
+}
---
+++ src/main/java/com/assignment/backend/constants/SuccessMessageConstants.java
@@ -0,0 +1,6 @@
+package com.assignment.backend.constants;
+
+public final class SuccessMessageConstants {
+    public static final String TOUR_PACKAGE_ADD_SUCCESSFULLY = "Tour Package Added Successfully!";
+    public static final String TOUR_PACKAGE_RETRIEVED_SUCCESSFULLY = "Tour Package Retrieved Successfully!";
+}
